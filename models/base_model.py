d35878c4f5ca756fdd9d6ed6cb5211196e68f36d
Jonah <jonahemmanuel54@gmail.com>
2023-08-09 11:19:09 -0700

    adding public instance class attribute ad methods

---
+++ models/base_model.py
@@ -0,0 +1,23 @@
+
+mport uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())  # Generate a unique ID
+        self.created_at = datetime.now()  # Set creation timestamp
+        self.updated_at = self.created_at  # Set initial update timestamp
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()  # Update the update timestamp
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()  # Copy instance attributes
+        obj_dict['__class__'] = self.__class__.__name__  # Add class name
+        obj_dict['created_at'] = self.created_at.isoformat()  # Convert to ISO format
+        obj_dict['updated_at'] = self.updated_at.isoformat()  # Convert to ISO format
+        return obj_dict
+
