fb3527b8cc6e0023fd8c2093730d3d2d462257e7
theenduranceA <theenduranceaneke@gmail.com>
2023-08-10 16:04:41 -0700

    Fixed errors

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
 Module containing a BaseModel class that defines
 all common attributes/methods for other classes.
@@ -7,15 +6,10 @@ all common attributes/methods for other classes.
 
 import uuid
 import models
-=======
-
-import uuid
->>>>>>> 38a86afa6350ca197938edadbd1927cc9cb850f2
 from datetime import datetime
 
 
 class BaseModel:
-<<<<<<< HEAD
     """A base class of public instance attributes."""
 
     def __init__(self, *args, **kwargs):
@@ -32,13 +26,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-=======
-    def __init__(self, *args, **kwargs)
-        if not kwargs:
-            self.id = str(uuid.uuid4())  # Get a unique ID
-            self.created_at = datetime.now()  # Set creation timestamp
-            self.updated_at = self.created_at  # Set initial update timestamp
->>>>>>> 38a86afa6350ca197938edadbd1927cc9cb850f2
 
     def __str__(self):
         """Print methods in string."""
