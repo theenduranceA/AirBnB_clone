7cdeab2c883c91441d4cc6fa92dab17aad09356b
theenduranceA <theenduranceaneke@gmail.com>
2023-08-10 11:58:34 -0700

    BaseModel updated for task 3 & 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,54 @@
+#!/usr/bin/python3
+"""
+Module containing a BaseModel class that defines
+all common attributes/methods for other classes.
+"""
 
-mport uuid
+import uuid
+import models
 from datetime import datetime
 
+
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())  # Generate a unique ID
-        self.created_at = datetime.now()  # Set creation timestamp
-        self.updated_at = self.created_at  # Set initial update timestamp
+    """A base class of public instance attributes."""
+
+    def __init__(self, *args, **kwargs):
+        """Instantiating the BaseModel class."""
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in "created_at" or key in "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key is not "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """Print methods in string."""
+
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now()  # Update the update timestamp
+        """
+        Public instance method that updates public
+        instance attribute with the current datetime.
+        """
+
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()  # Copy instance attributes
-        obj_dict['__class__'] = self.__class__.__name__  # Add class name
-        obj_dict['created_at'] = self.created_at.isoformat()  # Convert to ISO format
-        obj_dict['updated_at'] = self.updated_at.isoformat()  # Convert to ISO format
-        return obj_dict
+        """
+        Public instance method that returns a dictionary
+        containing all keys/values of the instance.
+        """
 
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
