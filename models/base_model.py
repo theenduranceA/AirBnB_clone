38a86afa6350ca197938edadbd1927cc9cb850f2
Jonah <jonahemmanuel54@gmail.com>
2023-08-10 10:55:08 -0700

    updatin base model iniit with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python3
 
-mport uuid
+import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())  # Generate a unique ID
-        self.created_at = datetime.now()  # Set creation timestamp
-        self.updated_at = self.created_at  # Set initial update timestamp
+    def __init__(self, *args, **kwargs)
+        if not kwargs:
+            self.id = str(uuid.uuid4())  # Get a unique ID
+            self.created_at = datetime.now()  # Set creation timestamp
+            self.updated_at = self.created_at  # Set initial update timestamp
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
