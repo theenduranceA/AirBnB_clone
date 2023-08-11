5a9d9bc547c7ffa888cb58bbd896ea752f5d8120
theenduranceA <theenduranceaneke@gmail.com>
2023-08-11 16:37:45 -0700

    Added functions

--- console.py
+++ console.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-"""
-created on Monday Auggust 09 2023 0520HRS
-
-Authors: Jonah Emmanuel and Endurance Aneke
-"""
+"""A module for the entry point of the command intepreter"""
 
 import cmd
-import model
+import models
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -16,15 +10,24 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 
+
 class HBNBCommand(cmd.Cmd):
-    """aclass that contains the entry point of the command interpreter.
-    """
+    """HBNBCommand class"""
+
     prompt = '(hbnb) '
 
-    def do_EOF(self, args):
-        """EOF command to exit the program.
-        """
+    def do_quit(self, line):
+        """Quit command to exit the program."""
         return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the program."""
+        return True
+
+    def emptyline(self):
+        """Nothing is executed with this command."""
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
