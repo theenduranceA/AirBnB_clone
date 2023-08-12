182a36973c67242133f287c1c5d88fb3cdef6f05
Jonah <jonahemmanuel54@gmail.com>
2023-08-12 09:20:47 -0700

    updating console.py with update comand

--- console.py
+++ console.py
@@ -124,6 +124,84 @@ class HBNBCommand(cmd.Cmd):
             return False
         print(to_print)
 
+    def default(self, args):
+        """Default method that is called when the inputted command starts
+        with a class name.
+
+        Attributes:
+            args (str): The inputted line string
+        """
+        line = args.strip('()').split(".")
+        if len(line) < 2:
+            print('** missing attribute **')
+            return
+        objects = models.storage.all()
+        class_name = line[0].capitalize()
+        cmd_name = line[1].lower()
+        split2 = cmd_name.strip(')').split('(')
+        cmd_name = split2[0]
+        if cmd_name == 'all':
+            HBNBCommand.do_all(self, class_name)
+        elif cmd_name == 'count':
+            count = 0
+            for k in objects.keys():
+                key = k.split('.')
+                if class_name == key[0]:
+                    count += 1
+            print(count)
+        elif cmd_name == 'show':
+            if len(split2) < 2:
+                print('** no instance found **')
+            else:
+                HBNBCommand.do_show(self, class_name + ' ' + split2[1])
+        elif cmd_name == 'destroy':
+            if len(split2) < 2:
+                print('** no instance found **')
+            else:
+                HBNBCommand.do_destroy(self, class_name + ' ' + split2[1])
+        elif cmd_name == 'update':
+            split3 = split2[1].split(', ')
+            if len(split3) == 0:
+                print('** no instance found **')
+            elif len(split3) == 1 and type(split3[1]) == dict:
+                for k, v in split[1].items():
+                    HBNBCommand.do_update(self, class_name + ' ' + split3[0] +
+                                          ' ' + k + ' ' + v)
+            elif len(split3) == 1 and type(split3[1]) != dict:
+                print('** no instance found **')
+            elif len(split3) == 2:
+                print('** no instance found **')
+            else:
+                HBNBCommand.do_update(self, class_name + ' ' + split3[0] +
+                                      ' ' + split3[1] + ' ' + split3[2])
+    def do_update(self, args):
+        """Updates an instance based on the class name and id
+        by adding or updating attribute (save the change into the JSON file).
+        """
+        line = args.split()
+        if not self.verify_class(line):
+            return
+        if not self.verify_id(line):
+            return
+        if not self.verify_attribute(line):
+            return
+        objects = models.storage.all()
+        key = '{}.{}'.format(line[0], line[1])
+        setattr(objects[key], line[2], line[3])
+        models.storage.save()
+
+    @staticmethod
+    def verify_attribute(line):
+        """Static method to verify the attribute in inputted line.
+        """
+        if len(line) < 3:
+            print("** attribute name missing **")
+            return False
+        elif len(line) < 4:
+            print("** value missing **")
+            return False
+        return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
