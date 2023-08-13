53796c55ce9d0f842ab534152a193209cd8f473d
theenduranceA <theenduranceaneke@gmail.com>
2023-08-13 15:17:27 -0700

    Added import function

--- console.py
+++ console.py
@@ -3,12 +3,15 @@
 
 import cmd
 import models
+from shlex import split
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
